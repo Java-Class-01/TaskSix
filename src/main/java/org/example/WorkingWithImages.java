c942f61fa5c8680c5b298c85ba6894c465c8cd6b
austin wamono <austinwamono98@gmail.com>
2025-10-25 06:44:29 +0300

    commit#2-creating the main frame prepareMainFruits()

--- src/main/java/org/example/WorkingWithImages.java
+++ src/main/java/org/example/WorkingWithImages.java
@@ -3,6 +3,21 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
 public class WorkingWithImages {
+    JFrame FruitsFrame;
+    JScrollPane scrollPane;
+    JPanel cardsPanel,FruitCardPanel;
+    public WorkingWithImages(){
+        this.prepareMainFruitsFrame();
+    }
+    public JFrame prepareMainFruitsFrame(){
+        FruitsFrame=new JFrame("Fruits Catalogue");
+        FruitsFrame.setSize(700,500);
+        FruitsFrame.add(this.prepareScrollPane());
+
+        FruitsFrame.setVisible(true);
+        return FruitsFrame;
+    }
+
 
 
 }
